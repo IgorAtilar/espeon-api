ba147944137c98699838f52ac841654145c4faec
Igor Atilar <igoratilar10@gmail.com>
2023-02-04 14:23:20 -0300

    :art: format index file

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.SERVER_PORT ?? 3000
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (_, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server started on port ${SERVER_PORT} 🤠👍`);
-});
+  console.log(`Server started on port ${SERVER_PORT} 🤠👍`)
+})
